bb71a4c147a77e5bd879e8fe9fd1a187eb9b8ed3
Gourav soni <gouravsoni8476@gmail.com>
2025-03-02 16:50:40 +0530

    feat: enhance Root UI components with improved styling and layout adjustments

--- apps/frontend/components/Appbar.tsx
+++ apps/frontend/components/Appbar.tsx
@@ -1,26 +1,27 @@
-import { Button } from "./ui/button";
 import {
-    ClerkProvider,
-    SignInButton,
-    SignUpButton,
-    SignedIn,
-    SignedOut,
-    UserButton,
-} from '@clerk/nextjs'
+	SignInButton,
+	SignUpButton,
+	SignedIn,
+	SignedOut,
+	UserButton,
+} from "@clerk/nextjs";
 
 export function Appbar() {
-  return (
-    <div className="flex justify-between px-2 py-1">
-      <div>Bolty</div>
-      <div>
-        <SignedOut>
-            <SignInButton />
-            <SignUpButton />
-        </SignedOut>
-        <SignedIn>
-            <UserButton />
-        </SignedIn>
-      </div>
-    </div>
-  );
+	return (
+		<div className="relative">
+			<div className="absolute bg-white blur-xl -z-10" />
+			<div className="w-full md:w-5/6 mx-auto flex justify-between border rounded-lg items-center bg-background px-4 py-1 md:py-2 md:px-6">
+				<h1 className="font-space-grotesk font-bold text-xl">Bolty</h1>
+				<div className="flex justify-center items-center size-12">
+					<SignedOut>
+						<SignInButton />
+						<SignUpButton />
+					</SignedOut>
+					<SignedIn>
+						<UserButton />
+					</SignedIn>
+				</div>
+			</div>
+		</div>
+	);
 }
